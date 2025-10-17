2b88185f15b2fa4ae693a2b5f1db4ea31ed0b2a9
Ihor222 <miskivigor10@gmail.com>
2025-10-17 20:01:45 +0300

    last

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,16 +1,25 @@
 import { HttpError } from "http-errors";
 
 export default function errorHandler(err, req, res, next) {
+  // Якщо це http-специфічна помилка (через createHttpError)
   if (err instanceof HttpError) {
-    res.status(err.status).json({
-      status: err.status,
-      message: err.message,
-    });
+    const response = {
+      status: err.status || 400,
+      message: err.message || "Bad Request",
+    };
+
+    // Якщо були додаткові дані (наприклад, err.errors з Joi)
+    if (err.errors) {
+      response.errors = err.errors;
+    }
+
+    res.status(response.status).json(response);
     return;
   }
 
+  // Інші (неочікувані) помилки
   res.status(500).json({
     status: 500,
-    message: "Internal Server Error",
+    message: err.message || "Internal Server Error",
   });
 }
